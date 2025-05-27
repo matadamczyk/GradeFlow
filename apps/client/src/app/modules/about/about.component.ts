a2624e07d5b4804250a82e349a9ed4dc6cb7cf01
Mateusz Adamczyk <madamczyk@student.agh.edu.pl>
2025-05-27 20:41:28 +0200

    feat(front-end): add `about`

--- apps/client/src/app/modules/about/about.component.ts
+++ apps/client/src/app/modules/about/about.component.ts
@@ -1,9 +1,16 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+// PrimeNG imports
+import { CardModule } from 'primeng/card';
+import { TagModule } from 'primeng/tag';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-about',
-  imports: [CommonModule],
+  standalone: true,
+  imports: [CommonModule, RouterModule, CardModule, TagModule, ButtonModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
 })
