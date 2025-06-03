83acf29cde7fdf11f1994fbfee5621af9cb2466f
PanJazdan <janpazdan@student.agh.edu.pl>
2025-06-03 15:42:04 +0200

    dodano findByTutor

--- apps/server/src/main/java/com/example/demo/dao/StudentClassRepository.java
+++ apps/server/src/main/java/com/example/demo/dao/StudentClassRepository.java
@@ -1,7 +1,10 @@
 package com.example.demo.dao;
 
 import com.example.demo.entity.StudentClass;
+import com.example.demo.entity.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface StudentClassRepository extends JpaRepository<StudentClass, Integer> {}
+public interface StudentClassRepository extends JpaRepository<StudentClass, Integer> {
+  StudentClass findByTutor(Teacher teacher);
+}
