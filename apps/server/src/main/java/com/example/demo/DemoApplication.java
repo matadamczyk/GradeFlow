0338412935512b3ef3ec1673e2a54dd0499b0f6b
BartlomiejRydzak <brydzak13@gmail.com>
2025-06-06 18:27:05 +0200

    add .env

--- apps/server/src/main/java/com/example/demo/DemoApplication.java
+++ apps/server/src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,19 @@
 package com.example.demo;
 
 // import org.springframework.boot.CommandLineRunner;
+
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
 
   public static void main(String[] args) {
+    Dotenv dotenv = Dotenv.load();
+    System.setProperty("DB_URL", dotenv.get("DB_URL"));
+    System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+    System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
     SpringApplication.run(DemoApplication.class, args);
   }
 
