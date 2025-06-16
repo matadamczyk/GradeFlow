197864baadd5fd4d2ae86e3404f7db3bdbfe2a65
Mateusz Adamczyk <madamczyk@student.agh.edu.pl>
2025-06-17 00:28:18 +0200

    feat(back-end): add role guards for `admin`

--- apps/server/src/main/java/com/example/demo/DemoApplication.java
+++ apps/server/src/main/java/com/example/demo/DemoApplication.java
@@ -29,10 +29,10 @@ public class DemoApplication {
 
   public static void main(String[] args) {
     // UNCOMMENT IF LAUNCHING LOCALLY
-    Dotenv dotenv = Dotenv.load();
-    System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
-    System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
-    System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+    // Dotenv dotenv = Dotenv.load();
+    // System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
+    // System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+    // System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
     SpringApplication.run(DemoApplication.class, args);
   }
 
