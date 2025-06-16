ade2ac28f69ccf7a64696be1907cf0eff7a3bce3
PanJazdan <janpazdan@student.agh.edu.pl>
2025-06-16 22:55:36 +0200

    feat(back-end): add role guards

--- apps/server/src/main/java/com/example/demo/dao/PresenceRepository.java
+++ apps/server/src/main/java/com/example/demo/dao/PresenceRepository.java
@@ -6,7 +6,9 @@ import com.example.demo.entity.Timetable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.sql.Date;
+import java.util.List;
 import java.util.Optional;
 
 public interface PresenceRepository extends JpaRepository<Presence, Integer> {
+  List<Presence> findByStudent(Student student);
 }
