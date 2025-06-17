dc401de23179a0fbcf7a4ad20b52590358ed4772
Mateusz Adamczyk <madamczyk@student.agh.edu.pl>
2025-06-17 22:31:52 +0200

    feat: wip

--- apps/server/src/main/java/com/example/demo/DemoApplication.java
+++ apps/server/src/main/java/com/example/demo/DemoApplication.java
@@ -29,10 +29,14 @@ public class DemoApplication {
 
   public static void main(String[] args) {
     // UNCOMMENT IF LAUNCHING LOCALLY
-    // Dotenv dotenv = Dotenv.load();
-    // System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
-    // System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
-    // System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+    Dotenv dotenv = Dotenv.load();
+    System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
+    System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+    System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+    System.setProperty("SPRING_MAIL_HOST", dotenv.get("SPRING_MAIL_HOST"));
+    System.setProperty("SPRING_MAIL_PORT", dotenv.get("SPRING_MAIL_PORT"));
+    System.setProperty("SPRING_MAIL_USERNAME", dotenv.get("SPRING_MAIL_USERNAME"));
+    System.setProperty("SPRING_MAIL_PASSWORD", dotenv.get("SPRING_MAIL_PASSWORD"));
     SpringApplication.run(DemoApplication.class, args);
   }
 
